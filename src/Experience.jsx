e0924b3429d9bb15aad1a6d8ebb00b7e5067a1d8
rfloarea <account@rfloarea.net>
2024-03-28 18:23:46 -0400

    basic Experience component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,58 @@
+import { useState } from "react"
 
 export default function Experience() {
+    const [position, setPosition] = useState('');
+    const [employer, setEmployer] = useState('');
+    const [description, setDescription] = useState('');
+
+    function handlePositionChange(event) {
+        setPosition(event.target.value);
+    }
+    function handleEmployerChange(event) {
+        setEmployer(event.target.value);
+    }
+    function handleDescriptionChange(event) {
+        setDescription(event.target.value);
+    }
+
     return (
-        <h2>Experience</h2>
+        <>
+            <div className="editor">
+                <h2>Experience</h2>
+                <label htmlFor="position">Position</label>
+                <input 
+                    type="text"
+                    id="position"
+                    value={position}
+                    onChange={handlePositionChange}
+                    placeholder="Your position" />
+                <label htmlFor="employer">Employer</label>
+                <input 
+                    type="text"
+                    id="employer"
+                    value={employer}
+                    onChange={handleEmployerChange}
+                    placeholder="Your employer" />
+                <label htmlFor="description"></label>
+                <textarea 
+                    name="description" 
+                    id="description"
+                    value={description}
+                    onChange={handleDescriptionChange} 
+                    cols="30" 
+                    rows="10"
+                    placeholder="Create new bullet points with 'shift + enter'"></textarea>
+            </div>
+
+            <div className="display">
+                <h3>{position}</h3>
+                <p>{employer}</p>
+                <ul>
+                    {description.split('\n').map((line, index) => 
+                        <li key={index}>{line}</li>)}
+                </ul>
+            </div>
+        
+        </>
     )
 }
