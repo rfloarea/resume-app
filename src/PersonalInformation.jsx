c5005e60e870465c2195131f0c89c1b31df11259
rfloarea <account@rfloarea.net>
2024-03-29 21:13:12 -0400

    email inputs

--- src/PersonalInformation.jsx
+++ src/PersonalInformation.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 
-// Common Parent Component
 export const PersonalInformation = () => {
   // Define shared state and update functions
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
 
   return (
     <div>
         <h2>Personal Information</h2>
         <FirstName firstName={firstName} setFirstName={setFirstName} />
         <LastName lastName={lastName} setLastName={setLastName} />
+        <Email email={email} setEmail={setEmail} />
     </div>
   );
 };
 
-// Component A: Input for First Name
 const FirstName = ({ firstName, setFirstName }) => {
   const handleChange = (event) => {
     setFirstName(event.target.value);
@@ -30,7 +30,6 @@ const FirstName = ({ firstName, setFirstName }) => {
   );
 };
 
-// Component B: Display First Name and Input for Last Name
 const LastName = ({ lastName, setLastName }) => {
   const handleLastNameChange = (event) => {
     setLastName(event.target.value);
@@ -40,7 +39,21 @@ const LastName = ({ lastName, setLastName }) => {
     <div>
         <label>First Name Input</label>
         <input type="text" value={lastName} onChange={handleLastNameChange} placeholder="Last Name" />
-        <p>Last Name Display {lastName}</p>
+        <p>Last Name Display: {lastName}</p>
     </div>
   );
 };
+
+const Email = ({ email, setEmail }) => {
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    return (
+        <div>
+            <label htmlFor="">Email Input</label>
+            <input type="text" value={email} onChange={handleEmailChange} placeholder='Email' />
+            <p>Email Display: {email} </p>
+        </div>
+    )
+}
